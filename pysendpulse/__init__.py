5a8e296015b3c8bae5f1c9e7457bb3dbd2da55b9
Maksym Ustymenko <m.ustymenko@sendpulse.com>
2017-09-13 08:22:52 +0000

    Update pypi package version

--- pysendpulse/__init__.py
+++ pysendpulse/__init__.py
@@ -2,4 +2,4 @@ __author__ = 'Maksym Ustymenko'
 __author_email__ = 'tech@sendpulse.com'
 __copyright__ = 'Copyright 2015, SendPulse'
 __credits__ = ['Maksym Ustymenko', ]
-__version__ = '0.0.1'
+__version__ = '0.0.2'
