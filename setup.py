378301b32760098a25c0ae0ffe94148bf105a07c
SendPulse <github@sendpulse.com>
2020-03-13 15:41:52 +0200

    Add long project description

--- setup.py
+++ setup.py
@@ -11,11 +11,16 @@ install_requires = ['python3-memcached', ]
 if version_info.major == 2:
     install_requires = ['python-memcached', 'requests', 'simplejson']
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
 setup(
     name='pysendpulse',
     version=__version__,
     packages=find_packages(),
     description='A simple SendPulse REST client library and example for Python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author=__author__,
     author_email=__author_email__,
     url='https://github.com/sendpulse/sendpulse-rest-api-python',
